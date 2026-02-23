9511fda196bfa71f486ebed6eb67e68c544cfbfe
danielmanning60 <danielmanning@me.com>
2026-02-23 13:21:37 +0000

    Add missing React entry point files

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+﻿function App() {
+  return (
+    <div className="App">
+      <h1>Watashi Bonsai</h1>
+      <p>Welcome to Watashi Bonsai - UK Bonsai Enthusiast Platform</p>
+    </div>
+  );
+}
+
+export default App;
